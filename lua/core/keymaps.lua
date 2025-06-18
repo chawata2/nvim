50aa1f1cc0887fb1d5b6aa5140446bfd8f62dafa
chawaRA <cha39881@gmail.com>
2025-06-18 20:20:17 +0900

    add barbar

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,3 +4,24 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode" })
 vim.keymap.set("n", "x", '"_x', { noremap = true, desc = "Delete character without copying" })
 vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { noremap = true, desc = "Clear search highlights" })
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Buffer
+-- Move to previous/next
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,22 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+
+		-- mdxを登録
+		vim.filetype.add({
+			extension = {
+				mdx = "mdx",
+			},
+		})
+		vim.treesitter.language.register('markdown', 'mdx')
+	end,
 }
+
