5bbd22a6ef1e4e9a3a451671777073c84860c1d0
chawaRA <cha39881@gmail.com>
2025-06-22 00:52:56 +0900

    update Telescope setting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,18 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim", {
+	dependencies = { "nvim-lua/plenary.nvim",
 		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-	} },
+	},
 	config = function()
+		require("telescope").setup({
+			defaults = {
+				sorting_strategy = "ascending",
+				layout_config = {
+					prompt_position = "top"
+				},
+			},
+		})
 		pcall(require("telescope").load_extension, "fzf")
 
 		local builtin = require("telescope.builtin")
